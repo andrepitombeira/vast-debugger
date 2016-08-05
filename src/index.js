f9979073c58447450f4af438c40a61d044c92f8c
Andre Pitombeira <andrepitombeira@gmail.com>
2016-08-05 16:58:53 +0200

    Import default styles

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import vastApp from './reducers';
 import App from './components/App';
+import 'index.css';
 
 let store = createStore(vastApp);
 
