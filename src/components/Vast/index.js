1de1571d118da689b583bf67f3dbb8f45e7349a4
Andre Pitombeira <andrepitombeira@gmail.com>
2016-08-05 17:00:28 +0200

    Create vast container component

---
+++ src/components/Vast/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const divStyle = {
+  border: 'solid 1px',
+  height: '200px'
+};
+
+const Vast = () => (
+  <div style={divStyle}>
+  </div>
+);
+
+export default Vast;
